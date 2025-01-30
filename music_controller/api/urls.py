28da65d02cd2ff485cd82d60ce19be5541859500
Anshu-Gondi <agondi982@gmail.com>
2025-01-31 00:02:52 +0530

    Django Rooms related function added

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import RoomView, CreateRoomView
+from .views import RoomView, CreateRoomView, GetRoom, JoinRoom, UserInRoom, LeaveRoom, UpdateRoom
 
 urlpatterns = [
-    path('rooms/', RoomView.as_view(), name='room-list-create'),
-    path('create-room/', CreateRoomView.as_view(), name='create-room'),
+    path('rooms/', RoomView.as_view(), name='room-list-create'),#DON't change
+    path('create-room/', CreateRoomView.as_view(), name='create-room'),#DON't change
+    # NEEDED TO FIX
+    path('get-room', GetRoom.as_view()),
+    path('join-room', JoinRoom.as_view()),
+    path('user-in-room', UserInRoom.as_view()),
+    path('leave-room', LeaveRoom.as_view()),
+    path('update-room', UpdateRoom.as_view()),
 ]
